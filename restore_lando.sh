c8602e38039921c53460df88d9bd5645e557258f
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-10-28 15:18:39 +0000

    Script Uploaded

---
+++ restore_lando.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+lando stop;
+
+lando poweroff;
+
+lando --clear;
+
+docker kill $(docker ps -q) ;
+
+docker rm $(docker ps -a -q) ;
+
+docker rmi $(docker images -q) ; 
+
+lando start ;
